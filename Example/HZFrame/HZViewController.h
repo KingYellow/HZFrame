229329f73d88e7e85e72a93a1d7e21368398ce38
黄振 <11222@qq.com>
2018-11-21 16:55:40 +0800

    Initial commit

---
+++ Example/HZFrame/HZViewController.h
@@ -0,0 +1,13 @@
+//
+//  HZViewController.h
+//  HZFrame
+//
+//  Created by KingYellow on 11/21/2018.
+//  Copyright (c) 2018 KingYellow. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HZViewController : UIViewController
+
+@end
---
+++ Example/HZFrame/HZViewController.m
@@ -0,0 +1,29 @@
+//
+//  HZViewController.m
+//  HZFrame
+//
+//  Created by KingYellow on 11/21/2018.
+//  Copyright (c) 2018 KingYellow. All rights reserved.
+//
+
+#import "HZViewController.h"
+
+@interface HZViewController ()
+
+@end
+
+@implementation HZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
